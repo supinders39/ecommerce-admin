32cf7d94ac64c2965df70e112358181dd7a41c7c
Supinder Singh <supinders39@gmail.com>
2023-10-08 04:49:51 +0530

    prisma, planet scale setup completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import ModalProvider from '@/providers/modal-provider'
+import ToasterProvider from '@/providers/toast-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,12 +16,14 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+  }) {
+  
   return (
     <ClerkProvider>
       <html lang="en">
       <body className={inter.className}>
-        <ModalProvider />
+        <ToasterProvider />
+          <ModalProvider />
         {children}
         </body>
     </html>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -9,6 +9,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 
 const formSchema = z.object({
@@ -17,6 +20,7 @@ const formSchema = z.object({
 
 const StoreModal = () => {
     const storeModal = useStoreModal();
+    const [loading, setLoading] = useState(false)
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -25,7 +29,16 @@ const StoreModal = () => {
     })
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            setLoading(true);
+            const response = await axios.post('/api/stores', values)
+            console.log(response.data)
+            toast.success("Store created.")
+         } catch (error) {
+            toast.error("Something went wrong")
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
@@ -46,7 +59,7 @@ const StoreModal = () => {
                                     <FormItem>
                                         <FormLabel>Name</FormLabel>
                                         <FormControl>
-                                            <Input placeholder="E-Commerce" {...field} />
+                                            <Input disabled={loading} placeholder="E-Commerce" {...field} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
@@ -54,8 +67,8 @@ const StoreModal = () => {
                             />
 
                             <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                                    <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-                                    <Button type="submit" >Continue</Button>
+                                    <Button disabled={loading} variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+                                    <Button disabled={loading} type="submit" >Continue</Button>
                             </div>
                         </form>        
                     </Form>
